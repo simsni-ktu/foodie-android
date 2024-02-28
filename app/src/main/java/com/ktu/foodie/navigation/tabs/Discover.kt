8e7fcde5f92e430c904d1ab5986ecdf4fc9a09b0
simsni-ktu <simonas.snioka@ktu.edu>
2024-02-28 22:53:04 +0200

    Create Tabs

---
+++ app/src/main/java/com/ktu/foodie/navigation/tabs/Discover.kt
@@ -0,0 +1,37 @@
+package com.ktu.foodie.navigation.tabs
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.rememberCameraPositionState
+
+@Composable
+fun Discover(){
+    val uiSettings by remember { mutableStateOf(MapUiSettings()) }
+
+    val properties by remember {
+        mutableStateOf(MapProperties(mapType = MapType.NORMAL))
+    }
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        GoogleMap(
+            cameraPositionState = cameraPositionState,
+            properties = properties,
+            uiSettings = uiSettings
+        )
+    }
+}
---
+++ app/src/main/java/com/ktu/foodie/navigation/tabs/Featured.kt
@@ -0,0 +1,14 @@
+package com.ktu.foodie.navigation.tabs
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Featured(){
+    Column(modifier = Modifier.fillMaxSize()){
+        Text("Featured")
+    }
+}
