64a18b263b4c242e53c24398707a7a6848fb0e38
simsni-ktu <simonas.snioka@ktu.edu>
2024-03-04 00:44:18 +0200

    Populate favorites tab with stale data

--- app/src/main/java/com/ktu/foodie/navigation/tabs/Favorites.kt
+++ app/src/main/java/com/ktu/foodie/navigation/tabs/Favorites.kt
@@ -1,14 +1,41 @@
 package com.ktu.foodie.navigation.tabs
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ktu.foodie.ui.compoonents.RestaurantCard
 import com.ktu.foodie.ui.compoonents.TopBar
 
 @Composable
-fun Favorites(){
-    Column(modifier = Modifier.fillMaxSize()){
+fun Favorites() {
+    Column {
         TopBar(homeScreen = false)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(start = 16.dp, end = 16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                userScrollEnabled = false,
+                modifier = Modifier.heightIn(max = 1000.dp)
+            ) {
+                items(6) {
+                    RestaurantCard()
+                }
+            }
+        }
     }
 }
