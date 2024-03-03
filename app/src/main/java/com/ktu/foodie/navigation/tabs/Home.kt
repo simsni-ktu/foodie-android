eae14b275abcfe7eec30bd44f9f7278bd7e5ef4a
simsni-ktu <simonas.snioka@ktu.edu>
2024-03-04 00:44:45 +0200

    Reformat Home.kt

--- app/src/main/java/com/ktu/foodie/navigation/tabs/Home.kt
+++ app/src/main/java/com/ktu/foodie/navigation/tabs/Home.kt
@@ -43,32 +43,37 @@ fun Home() {
 
     val categories = listOf("Sweets", "Healthy", "Fruits", "Bakery", "Vegetarian", "Dairy", "Dairy", "Dairy")
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(start = 16.dp, end = 16.dp, top = 24.dp)
-    ) {
-        TopBar(homeScreen = true)
-        Spacer(modifier = Modifier.height(24.dp))
-        WelcomeCard(username = "Bob", title = "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum!!")
-        Spacer(modifier = Modifier.height(16.dp))
-        CurrentOrder(allOrders = {})
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
-            Text("Categories", fontWeight = FontWeight.Bold, color = foodieDarkGreen)
+    Column {
+        TopBar(homeScreen = false)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(start = 16.dp, end = 16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(24.dp))
+            WelcomeCard(
+                username = "Bob",
+                title = "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum!!"
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            CurrentOrder(allOrders = {})
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+                Text("Categories", fontWeight = FontWeight.Bold, color = foodieDarkGreen)
+            }
+            LazyVerticalGrid(
+                modifier = Modifier.heightIn(max = 1000.dp),
+                contentPadding = PaddingValues(vertical = 16.dp),
+                userScrollEnabled = false,
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                content = {
+                    items(categories) { category ->
+                        CategoryCard(title = category)
+                    }
+                })
         }
-        LazyVerticalGrid(
-            modifier = Modifier.heightIn(max = 1000.dp),
-            contentPadding = PaddingValues(vertical = 16.dp),
-            userScrollEnabled = false,
-            columns = GridCells.Fixed(2),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            content = {
-                items(categories) { category ->
-                    CategoryCard(title = category)
-                }
-            })
     }
 }
