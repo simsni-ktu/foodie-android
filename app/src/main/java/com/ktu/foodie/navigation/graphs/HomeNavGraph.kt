ed0f92b97d952ef563f5dceed149fa9ee2785ff7
simsni-ktu <simonas.snioka@ktu.edu>
2024-03-09 19:55:32 +0200

    Create separate navgraph for tabs

---
+++ app/src/main/java/com/ktu/foodie/navigation/graphs/HomeNavGraph.kt
@@ -0,0 +1,41 @@
+package com.ktu.foodie.navigation.graphs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.ktu.foodie.navigation.Graph
+import com.ktu.foodie.navigation.Tabs
+import com.ktu.foodie.navigation.tabs.Account
+import com.ktu.foodie.navigation.tabs.Discover
+import com.ktu.foodie.navigation.tabs.Favorites
+import com.ktu.foodie.navigation.tabs.Featured
+import com.ktu.foodie.navigation.tabs.Home
+
+@Composable
+fun HomeNavGraph(bottomBarNavController: NavHostController, rootNavController: NavController, modifier: Modifier) {
+    NavHost(
+        navController = bottomBarNavController,
+        route = Graph.BOTTOM_NAVIGATION,
+        startDestination = Tabs.Home.route,
+        modifier = modifier
+    ){
+        composable(Tabs.Home.route) {
+            Home()
+        }
+        composable(Tabs.Featured.route) {
+            Featured()
+        }
+        composable(Tabs.Discover.route) {
+            Discover()
+        }
+        composable(Tabs.Favorites.route) {
+            Favorites()
+        }
+        composable(Tabs.Account.route) {
+            Account(logout = { rootNavController.navigate(Graph.ROOT) })
+        }
+    }
+}
