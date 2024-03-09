2c93a883dd7185b7a76a63ce0161b58716159604
simsni-ktu <simonas.snioka@ktu.edu>
2024-03-09 19:55:58 +0200

    UI adjustments in Account tab

--- app/src/main/java/com/ktu/foodie/navigation/tabs/Account.kt
+++ app/src/main/java/com/ktu/foodie/navigation/tabs/Account.kt
@@ -2,15 +2,28 @@ package com.ktu.foodie.navigation.tabs
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ktu.foodie.ui.compoonents.Button
 import com.ktu.foodie.ui.compoonents.TopBar
 
 @Composable
-fun Account(){
-    Column(modifier = Modifier.fillMaxSize()){
+fun Account(logout:() -> Unit){
+    Column {
         TopBar(homeScreen = false)
-
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(start = 16.dp, end = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(title = "Logout", onClick = {logout()})
+        }
     }
 }
