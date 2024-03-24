ac71bcde10cc066e0a711249f71e1bf4c533fee1
simsni-ktu <simonas.snioka@ktu.edu>
2024-03-25 00:04:27 +0200

    Add tabs for restaurant

---
+++ app/src/main/java/com/ktu/foodie/navigation/tabs/restaurant/AddBox.kt
@@ -0,0 +1,26 @@
+package com.ktu.foodie.navigation.tabs.restaurant
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ktu.foodie.ui.compoonents.Button
+import com.ktu.foodie.ui.compoonents.TopBar
+
+@Composable
+fun AddBox() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(start = 16.dp, end = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+    }
+
+}
