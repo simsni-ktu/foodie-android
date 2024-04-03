f985199778d9dfcbf245cfacdda683f3116fc3bd
simsni-ktu <simonas.snioka@ktu.edu>
2024-04-03 21:56:11 +0300

    Add new routes to BackendInterface

--- app/src/main/java/com/ktu/foodie/api/BackendInterface.kt
+++ app/src/main/java/com/ktu/foodie/api/BackendInterface.kt
@@ -1,10 +1,15 @@
 package com.ktu.foodie.api
 
+import com.ktu.foodie.api.models.FoodItem
+import com.ktu.foodie.api.models.FoodPackage
+import com.ktu.foodie.api.models.FoodPackagePost
 import com.ktu.foodie.api.models.Message
 import com.ktu.foodie.api.models.UserCredentials
 import ktu.foodie.proto.User
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface BackendInterface {
 
@@ -17,4 +22,19 @@ interface BackendInterface {
     suspend fun register(
         @Body userCredentials: UserCredentials
     ): Message
+
+    @POST("food-item")
+    suspend fun postFoodItem(
+        @Body foodItem: FoodItem
+    ): FoodItem
+
+    @GET("food-item/{userId}")
+    suspend fun getFoodItems(
+        @Path("userId") userId: Int
+    ): List<FoodItem>
+    
+    @POST("food-packages")
+    suspend fun postFoodPackage(
+        @Body foodPackage: FoodPackagePost
+    ): FoodPackage
 }
